1ebf8f3acf1ec3e12b1f61aa125cee94999094fd
sixorca00-collab <sixorca00@gmail.com>
2026-03-12 21:25:00 -0500

    Ejercicios inicializando strings

---
+++ 01-inicios/Strings.java
@@ -0,0 +1,18 @@
+public class Strings {
+    public static void main(String[] args) {
+        //Strings.
+    var name = new String("SixOrca");
+    String Frase = "testeo de subString";
+        //Concatenación: Ya la habia visto en ejercicios previos.
+        //Length
+        System.out.println(name.length());// debe dar 7. DA SIETEEEE
+        //Obtener un caracter
+        System.out.println(name.charAt(name.length()-1)); // Asi se obtiene una "S" y con -1 es el ultimo caracter.
+        //SubString
+        System.out.println(Frase.substring(0, 6));
+        //The CASES toUperCase-toLowerCase.- Transform a mayus y minus
+        
+
+
+    }
+}
